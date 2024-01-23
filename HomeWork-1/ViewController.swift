34097abac8401619532605e356a0c4a5f928391f
Vlad <mackva34@gmail.com>
2024-01-23 11:59:54 +0100

    Initial Commit

---
+++ HomeWork-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork-1
+//
+//  Created by Владислав Соколов on 23.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
